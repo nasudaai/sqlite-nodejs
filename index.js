ec028134708f8362462c6bde29e42ffdee2c14a7
nasuda <nasuda908@gmail.com>
2023-11-23 22:06:12 +0900

    add index.js 'db.all'

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ let db = new sqlite3.Database('./db/sample.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('connected to the in-memory sqlite database.');
+  console.log('connected to the sample.db sqlite database.');
 });
 
 //console.log(db); -> database {}
+
+db.all('select * from sample', [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+    
+    console.log(rows);
+});
 
 db.close((err) => {
   if (err) {
